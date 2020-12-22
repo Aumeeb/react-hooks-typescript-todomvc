e829d77a6db3b3b1162e9eff27c8a5360b829b5a
noberk <noberk@users.noreply.github.com>
2020-12-22 16:21:40 +0800

    add : a function that enable to generate short id

---
+++ src/utils/gen.ts
@@ -0,0 +1,8 @@
+type ShortUniqueId = string
+export function shortId(): ShortUniqueId {
+  let firstPart: any = (Math.random() * 46656) | 0
+  let secondPart: any = (Math.random() * 46656) | 0
+  firstPart = ('000' + firstPart.toString(36)).slice(-3)
+  secondPart = ('000' + secondPart.toString(36)).slice(-3)
+  return firstPart + secondPart
+}
